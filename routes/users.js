12efd24dde93b3be22f590364cf55c17abfb01a0
reddynie11 <reddy.nie11@gmail.com>
2019-11-12 18:13:44 +0530

    a

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 
+//Schema
+const User = require('../models/User');
+
 //@router       POST api/users
 //desc          Register a user
 //access        Public
 router.post('/',(req,res)=>{
-    res.send('Register a user')
+    res.send(req.body)
 });
 
 module.exports= router;
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
+
+//database connextion
+connectDB();
+//middleware
+app.use(express.json({ extended : false }))
 
 app.get('/', (req,res)=>{
     res.send('home')
