345ee0e4ef78248bf61ea391b9adcc7de69091e7
reddynie11 <reddy.nie11@gmail.com>
2019-11-12 18:54:00 +0530

    user saved to db

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
@@ -14,11 +15,31 @@ router.post('/',
                 check('email','Enter a valid email').isEmail(),
                 check('password','password should be min. 6 charcters').isLength({min: 6})
             ],
-            (req,res)=>{
+            async (req,res)=>{
                 const errors = validationResult(req);
                 if(!errors.isEmpty()){
                     return res.status(400).json({ errors : errors.array() });
                 }
+                const {name, email, password} = req.body;
+                try{
+                    let user = await User.findOne({ email : email });
+                    if (user){
+                        return res.status(400).json({msg : 'User already exists'});
+                    }
+                    user = new User({
+                        name: name,
+                        email: email,
+                        password: password
+                    });
+                    const salt = await bcrypt.genSalt(10);
+                    user.password = await bcrypt.hash(password, salt);
+                    await user.save()
+                    res.send(user) 
+                } catch (err) {
+                    console.error(err.message)
+                    res.send('error')
+                }
+
                 res.send(req.body)
             });
 
