59e808b45f3ae8de6518a12fa2fcfdf4d1deb6fe
reddynie11 <reddy.nie11@gmail.com>
2019-11-12 20:47:54 +0530

    contact model & add contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+//Schema
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 //@router       GET api/constacts
 //desc          get user contacts
 //access        Private
-router.get('/',(req,res)=>{
+router.get('/',auth, async (req,res)=>{
+    try{
+        const contacts= await Contact.find({user: req.user.id});
+        res.json(contacts)  
+    }catch (err){
+        console.error(err.message)
+        res.status(500).send('Server error')
+    }
+    
     res.send('get all user contacts')
 });
 
 //@router       POST api/contacts
 //desc          Add contacts
 //access        Private
-router.get('/',(req,res)=>{
-    res.send('Add contacts')
-});
+router.post('/', [auth, [check('name', 'Name is required').not().isEmpty()]], 
+                async (req,res)=>{
+                    const errors = validationResult(req);
+                    if(!errors.isEmpty()){
+                        return res.status(400).json({ errors : errors.array() });
+                    }
+                    const {name, email, phone, type} = req.body;
+
+                    try{
+                        const newContact = new Contact({
+                            name: name,
+                            email: email,
+                            phone: phone,
+                            type: type,
+                            user: req.user.id
+                        });
+                        const contact = await newContact.save();
+                        res.json(contact)
+                    }catch(err){
+                        console.error(err.message)
+                        res.status(500).send('Server error')
+                    }
+                }
+            );
 
 //@router       PUT api/contacts/:id
 //desc          update contact
