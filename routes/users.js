a6225179bcf5a5321f06c15212c5d28d8be25ccb
reddynie11 <reddy.nie11@gmail.com>
2019-11-12 18:34:54 +0530

    user model + validation

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,25 @@
 const express = require('express');
 
 const router = express.Router();
-
+const { check, validationResult } = require('express-validator');
 //Schema
 const User = require('../models/User');
 
 //@router       POST api/users
 //desc          Register a user
 //access        Public
-router.post('/',(req,res)=>{
-    res.send(req.body)
-});
+router.post('/',
+            [
+                check('name','Name is required').not().isEmpty(),
+                check('email','Enter a valid email').isEmail(),
+                check('password','password should be min. 6 charcters').isLength({min: 6})
+            ],
+            (req,res)=>{
+                const errors = validationResult(req);
+                if(!errors.isEmpty()){
+                    return res.status(400).json({ errors : errors.array() });
+                }
+                res.send(req.body)
+            });
 
 module.exports= router;
